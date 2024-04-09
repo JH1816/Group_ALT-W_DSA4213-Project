1949ff3c27c2ac4d2ff8999ed18109f9729c7568
alvinbong <eeechun2002@gmail.com>
2024-04-09 17:13:29 +0800

    modify the rag and data ingested, and create a new collection id

--- rag/data.py
+++ rag/data.py
@@ -1,22 +1,16 @@
 import requests
 import pandas as pd
+from json import loads, dumps
+import pdfkit
+import glob
 
 # Wikipedia Movie Data
-
-# Base URL for retrieving movie data
-Wikipedia_movie_data_URL = "https://github.com/prust/wikipedia-movie-data/raw/master/movies-{}s.json"
-
-import pandas as pd
-import requests
-
-# Wikipedia Movie Data
-def wikipedia_movie_data(start_year: int = 1960, end_year: int = 2020):
-    """Fetches Wikipedia movie data for specified years.
-    
-    Parameters:
-    start_year: Starting year of the movie required
-    end_year: Ending year of the movie required
+def wikipedia_movie_data():
     """
+    Obtained movie data from Wikipedia from 1960 to 2020.
+    """
+    start_year = 1960
+    end_year = 2020
     
     # Generating list of movie years of interest
     list_of_year_of_interest = []
@@ -41,20 +35,46 @@ def wikipedia_movie_data(start_year: int = 1960, end_year: int = 2020):
     
     # Concatenating all DataFrames and returning the combined DataFrame
     final_data = pd.concat(data_results, axis=0)
+    # Select columns that are useful for the data ingestion
+    final_data = final_data[["title","year","genres","extract"]]
+    final_data['genres'] = final_data['genres'].apply(lambda x: ', '.join(map(str, x)))
+    final_data.rename(columns={'extract': 'additional information','title':'movie title','year':'release year'}, inplace=True)
 
     return final_data
 
 ## MovieLens Data
 def movielens_data():
-    movielens_data = pd.read_csv("../data/ml-25m/movies.csv")
-    tags_data = pd.read_csv("../data/ml-25m/tags.csv")
+    """
+    Preprocess movielens_data.
+    """
+    movielens_data = pd.read_csv("../data/movies.csv")
+    tags_data = pd.read_csv("../data/tags.csv")
     tags_data = tags_data[['movieId','tag']]
     grouped_tags_data = tags_data.groupby('movieId')['tag'].agg(list).reset_index()
+    grouped_tags_data['tag'] = grouped_tags_data['tag'].apply(lambda x: ', '.join(map(str, x)))
     final_movie_data = pd.merge(movielens_data,grouped_tags_data,on="movieId",how='left')
+    final_movie_data[['movie title', 'release year']] = final_movie_data['title'].str.extract(r'([^\(]+)\s*\((\d{4})\)')
+    final_movie_data.rename(columns={'tag': 'additional information'}, inplace=True)
+    final_movie_data = final_movie_data[["movie title","release year","genres","additional information"]]
     return final_movie_data
 
-movielens_data = movielens_data()
-Wikipedia_movie_data = wikipedia_movie_data()
+def process_data_for_ingestion():
+    """
+    Combine movie data of Wikipedia and Movie Lens.
+    """
+    movielens_data_ = movielens_data()
+    Wikipedia_movie_data = wikipedia_movie_data()
+    final_data = pd.concat([movielens_data_,Wikipedia_movie_data],axis=0)
+    final_data = final_data.reset_index(drop=True)
+    return final_data
 
-# movielens_data.to_excel("movielens_data.xlsx")
-# Wikipedia_movie_data.to_excel("Wikipedia_movie_data.xlsx")
+# Below is to convert dataframe to html
+# data_for_ingestion = process_data_for_ingestion()
+# data_for_ingestion.to_html('movie_data_for_ingestion.html')
+
+# Below is to convert html to pdf
+# config = pdfkit.configuration(wkhtmltopdf='C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe')
+# for file in glob.glob('./*.html'):
+#     pdfkit.from_file(file, file[:-4]+'.pdf', configuration=config)
+
+
--- rag/rag.py
+++ rag/rag.py
@@ -1,55 +1,53 @@
 from h2ogpte import H2OGPTE
 
-prompt = """
+prompt = '''
+    As a movie enthusiast with expertise in the field, your task is to utilize your 
+    knowledge and the given information to analyze and offer tailored movie
+    suggestions that align with the preferences of the person seeking recommendations.
 
-You are an expert in movies. Using your knowledge and based on the details provided
-below, analyse do your best to provide movie recommendations that will appease
-to the person asking for movie recommendations.
+    The person has a preferred genre: {0}. \
+    The person has a preferred years of releases: {1} to {2}. \
+    User favourite movie: {3}
+    Recommend list of movies using below formatting:
 
-The person has a preferred genre: {0}. \
-The person has a preferred years of releases: {1}. \
-Recommend list of candidate movies: []=.\
-Return a list of boolean values and explain why the person likes or dislikes.
+    << FORMATTING >>
+    Return a markdown code snippet with a list of JSON object formatted as below:
+    {{
+        "title": string \ Denotes the title of a movie
+        "released year": integer \ Denotes the released year of the movie
+        "genre": string \ Denotes the genre of the movie
+        "like": boolean \ true or false
+        "explanation": string \ Provide an explanation about why this person likes or dislikes this movie.
+    }}
 
-<< FORMATTING >>
-Return a markdown code snippet with a list of JSON object formatted to look like:
-{{
-    "title": string \ the name of the movie in candidate movies
-    "like": boolean \ true or false
-    "explanation": string \ explain why the person likes or dislikes the candidate movie
-}}
+    Important Note: Provide a boolean value and an explanation for each movie in the list of recommended movies.
+    Important Note: The explanation must be relevant to the individual's preferences for liked and disliked movies.
+    '''
 
-REMEMBER: Each boolean and explanation for each element in candidate movies.
-REMEMBER: The explanation must relate to the person's liked and disliked movies.
-"""
-
-prompt1 = prompt.format("Horror","2000 to 2010")
-prompt2 = prompt.format("fantasy","2010 to 2020")
+prompt1 = prompt.format("Horror","2000","2010","Playback")
+prompt2 = prompt.format("fantasy","2010", "2020", "Toy Story")
 
 client = H2OGPTE(
     address='https://h2ogpte.genai.h2o.ai',
-    api_key='sk-xxx',       
+    api_key='sk-Xe7ocXvl1gW4HzLnMTh8QQuIutmb6OwBAHF2sQvCgxbbeSB2',       
 )
 
 # Create a new collection
 collection_id = client.create_collection(
-    name='Movie Recommendation System',
+    name='Movie Recommendation System_Collection',
     description='To enhance the movie-watching experience, we propose a movie recommendation system which uses data analytics and advanced machine-learning techniques to deliver personalized movie suggestions',
 )
 
 # Upload documents
 # Many file types are supported: text/image/audio documents and archives
 
-with open('movielens_data.xlsx', 'rb') as f:
-    movielens_data = client.upload('movielens_data.xlsx', f)
-    
-with open('Wikipedia_movie_data.xlsx', 'rb') as f:
-    Wikipedia_movie_data = client.upload('Wikipedia_movie_data.xlsx', f)
+with open('movie_data_for_ingestion.pdf', 'rb') as f:
+    data_for_ingestion = client.upload('movie_data_for_ingestion.pdf', f)
 
-client.ingest_uploads(collection_id, [movielens_data, Wikipedia_movie_data])
+client.ingest_uploads(collection_id, [data_for_ingestion])
 
 # Create a chat session
-chat_session_id = client.create_chat_session(collection_id)
+chat_session_id = client.create_chat_session('f6ea3a8f-143f-4006-9be3-a74054e96a00')
 
 # Query the collection
 with client.connect(chat_session_id) as session:
